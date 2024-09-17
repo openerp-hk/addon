3bf0787b638b077a6c8931368cd7aba4e16e2d56
openerp-hk <139341727+openerp-hk@users.noreply.github.com>
2024-09-17 23:15:07 +0800

    Add files via upload

---
+++ stock_serial_number/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Row numbers in Stock",
+
+    'summary': "Control the visibility of row numbers in stock",
+
+    'description': """
+    This add-on is a functional plug-in that allows users to control the visibility of row numbers in the stock modules.
+    """,
+
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'author': "Odoo.RED team",
+    'website': "https://cdn.odoo.red",
+    'category': 'Extra Tools',
+    'version': '17.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock', 'product', 'web_list_serial_number'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/serialNumberListView.xml',
+    ],
+    # only loaded in demonstration mode
+    'assets': {
+        'web.assets_backend': [
+            "stock_serial_number/static/src/views/*/*.*",
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/images/stock.jpg'],
+    'sequence': 100
+}
+
