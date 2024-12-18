5615625e621e218fe5b151974f1918b69f038a1b
openerp-hk <139341727+openerp-hk@users.noreply.github.com>
2024-12-18 21:37:44 +0800

    Update __manifest__.py

--- keyboard_subtree/__manifest__.py
+++ keyboard_subtree/__manifest__.py
@@ -9,7 +9,7 @@
         Frees your hands from the mouse, boosts operation efficiency.  
     """,
 
-    'author': "Josh.Feng, Openerp.HK, Odoo.Red, Odoo SA",
+    'author': "Odoo.Red team, Odoo SA, Josh.Feng",
     'website': "https://cdn.odoo.red/",
 
     # Free your hands from the mouse!
